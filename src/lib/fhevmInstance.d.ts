615b10b8ac34ad820297e4184bf9cb29c6bb682b
bojack <wangchao@msn.cn>
2025-12-24 22:33:05 +0800

    Initial commit

---
+++ src/lib/fhevmInstance.d.ts
@@ -0,0 +1,20 @@
+// Type declarations for fhevmInstance.js
+
+export interface EncryptedInput {
+  encryptedData: string;
+  proof: string;
+}
+
+export interface DecryptMultipleResult {
+  cleartexts: string;
+  decryptionProof: string;
+  values: number[];
+}
+
+// Main exports
+export declare function decryptValue(handle: string, contractAddress: string, signer: any): Promise<number>;
+export declare function createEncryptedInput(contractAddress: string, userAddress: string, value: number): Promise<EncryptedInput>;
+export declare function publicDecrypt(encryptedData: string): Promise<number>;
+export declare function decryptMultipleHandles(contractAddress: string, signer: any, handles: string[]): Promise<DecryptMultipleResult>;
+export declare function initializeFheInstance(): Promise<any>;
+export declare function getFheInstance(): any;
