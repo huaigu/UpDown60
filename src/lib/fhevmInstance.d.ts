df9bb8cf0e08386769ad5fbae602f5a5d9654cad
bojack <wangchao@msn.cn>
2025-12-27 14:53:13 +0800

    Expose fetchPublicDecryption typing

--- src/lib/fhevmInstance.d.ts
+++ src/lib/fhevmInstance.d.ts
@@ -16,5 +16,6 @@ export declare function decryptValue(handle: string, contractAddress: string, si
 export declare function createEncryptedInput(contractAddress: string, userAddress: string, value: number): Promise<EncryptedInput>;
 export declare function publicDecrypt(encryptedData: string): Promise<number>;
 export declare function decryptMultipleHandles(contractAddress: string, signer: any, handles: string[]): Promise<DecryptMultipleResult>;
+export declare function fetchPublicDecryption(handles: string[]): Promise<any>;
 export declare function initializeFheInstance(): Promise<any>;
 export declare function getFheInstance(): any;
